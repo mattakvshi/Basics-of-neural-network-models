542045b822391dd723e3aa53d08718321d1b76d2
mattakvshi <ytube9504@gmail.com>
2023-10-16 11:45:47 +0300

    bug fix

--- 34_sidorenko_gpt5/34_sidorenko_gpt5/MainForm.Designer.cs
+++ 34_sidorenko_gpt5/34_sidorenko_gpt5/MainForm.Designer.cs
@@ -267,6 +267,7 @@
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(232, 20);
             this.numericUpDown1.TabIndex = 16;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // TestButton
             // 
--- 34_sidorenko_gpt5/34_sidorenko_gpt5/MainForm.cs
+++ 34_sidorenko_gpt5/34_sidorenko_gpt5/MainForm.cs
@@ -7,13 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace _34_sidorenko_gpt5
 {
     public partial class MainForm : Form { 
 
         int[] statusArray = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    
+        int numericValue = 0;  // new variable to hold the numericUpDown value
+
         public MainForm()
         {
             InitializeComponent();
@@ -48,84 +50,101 @@ namespace _34_sidorenko_gpt5
 
         private void button2_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button2, 0);
+            ChangeStatusData(button2, 1);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button3, 0);
+            ChangeStatusData(button3, 2);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button4, 0);
+            ChangeStatusData(button4, 3);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button5, 0);
+            ChangeStatusData(button5, 4);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button6, 0);
+            ChangeStatusData(button6, 5);
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button7, 0);
+            ChangeStatusData(button7, 6);
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button8, 0);
+            ChangeStatusData(button8, 7);
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button9, 0);
+            ChangeStatusData(button9, 8);
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button10, 0);
+            ChangeStatusData(button10, 9);
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button11, 0);
+            ChangeStatusData(button11, 10);
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button12, 0);
+            ChangeStatusData(button12, 11);
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button13, 0);
+            ChangeStatusData(button13, 12);
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
-            ChangeStatusData(button14, 0);
+            ChangeStatusData(button14, 13);
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
 
-            ChangeStatusData(button15, 0);
+            ChangeStatusData(button15, 14);
         }
 
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            // Convert the decimal value from the numericUpDown1 to an int and store it in numericValue
+            numericValue = Decimal.ToInt32(numericUpDown1.Value);
+        }
 
         private void LerningButton_Click(object sender, EventArgs e)
         {
-             
+            // Convert the statusArray into a comma separated string
+            string output = numericValue + "-" + string.Join(",", statusArray);
+
+            // Append the new array to the end of the file, with a newline
+            // If the file does not exist, this method creates it
+            File.AppendAllText("Lerning.txt", output + Environment.NewLine);
         }
 
         private void TestButton_Click(object sender, EventArgs e)
         {
+            // Convert the statusArray into a comma separated string
+            string output = numericValue + "-" + string.Join(",", statusArray);
 
+            // Append the new array to the end of the file, with a newline
+            // If the file does not exist, this method creates it
+            File.AppendAllText("Test.txt", output + Environment.NewLine);
         }
+
+
     }
 }
